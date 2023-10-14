9cd7f4c151a4163019ecb4abdaede036fcb80adc
0-Reker-0 <147882553+0-Reker-0@users.noreply.github.com>
2023-10-14 19:09:11 +0300

    lol_kek_azazaz

--- scripts.js
+++ scripts.js
@@ -1,21 +1,18 @@
-/*document.getElementById('login').onclick = function(){
-	document.getElementById('entrance').innerHTML = '';
-}*/
 $(document).ready(function() {
 	$("#login").click(function() {
-		$("#main").html('<div id = "login_form">'+
-		'<h1>Войти</h1>'+
-		'<form action = "login.php" method = "POST">'+
-			'<p><input type = "text" name = "username" class = "text" required placeholder = "Введите логин"></p>'+
-			'<p><input type = "text" name = "password" class = "text" required placeholder = "Введите пароль"></p>'+
-			'<p><input type = "text" name = "code-word" class = "text" required placeholder = "Введите код-слово"></p>'+
-			'<p></p><input type = "submit" name = "submit" class = "button" id = "subm_logn" value = "Войти"></p>'+
-		'</form>'+
-	'</div>');
+		$("#main").css("display", "none");
+		$("#login_form, #navigation").css("display", "block")
 	});
-	$("#registr").click(function() {
-		$("#main").html('');
+	$("#registr, #registr_frm_logn").click(function() {
+		$("#main, #login_form").css("display", "none");
+		$("#reg_form, #navigation").css("display", "block")
+	});
+	$("#email_enter").click(function() {
+		$("#login_form").css("display", "none");
+		$("#email_form, #navigation").css("display", "block")
+	});
+	$("#back").click(function() {
+		$("#reg_form, #login_form, #email_form, #navigation").css("display", "none");
+		$("#main").css("display", "block")
 	});
 });
-//lol idi na huy))))
-//timlid pidor!
